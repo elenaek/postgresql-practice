5afc4319c943f3b16c7279b3004ce25f5f12dcd4
apocdm <keanele12345@gmail.com>
2025-02-02 20:49:22 +0000

    add script for showing roles on a db, add create-role script

---
+++ sql-scripts/DDL/create-role.sql
@@ -0,0 +1,15 @@
+-- Creates a login role which has superuser permissions (basically able to do anything) that expires at some date
+-- and a group role which has superuser permissions
+-- Probably want to delete 
+
+CREATE ROLE superman
+LOGIN -- Attribute that allows the user to login, and is what separates a LOGIN role from a GROUP role
+PASSWORD 'notSoSuperSecretPass!'
+CREATEDB -- Attribute that allows the user to create databases (redundant with superuser)
+SUPERUSER -- Attribute that gives the user superuser privs
+VALID UNTIL '2025-03-02'; -- Attribute that expires the password on specified date
+
+CREATE ROLE supermen -- Creates a group role as this statement does not have the LOGIN attribute
+PASSWORD 'notSoSuperSecretPass!'
+CREATEDB 
+SUPERUSER;
---
+++ sql-scripts/DML/show-roles.sql
@@ -0,0 +1,4 @@
+-- Displays all of the roles in a PostgreSQL DB
+
+SELECT rolname
+FROM pg_roles;
