976ca97654488a1c7aa7fae776e2371a32a1858f
apocdm <keanele12345@gmail.com>
2025-02-02 21:03:52 +0000

    update init and reinit scripts

--- init-sql.sh
+++ init-sql.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-createdb $PGDATABASE && echo "Database: $PGDATABASE created." && pg_restore -U $PGUSER -d $PGDATABASE sql-init/dvdrental.tar && echo "Sample data restored into $PGDATABASE.";
+createdb $PGDATABASE && 
+echo "Database: $PGDATABASE created." &&
+pg_restore -U $PGUSER -d $PGDATABASE sql-init/dvdrental.tar &&
+echo "Sample data restored into $PGDATABASE.";
--- reinit-sql.sh
+++ reinit-sql.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-dropdb $PGDATABASE && echo "Database: $PGDATABASE deleted." && createdb $PGDATABASE && echo "Database: $PGDATABASE created." && pg_restore -U $PGUSER -d $PGDATABASE sql-init/dvdrental.tar && echo "Sample data restored into $PGDATABASE.";
+# dropdb --force attempts to terminate all connections to the db before dropping it
+dropdb --force $PGDATABASE && echo "Database: $PGDATABASE deleted." && 
+createdb $PGDATABASE && 
+echo "Database: $PGDATABASE created." && 
+pg_restore -U $PGUSER -d $PGDATABASE sql-init/dvdrental.tar &&
+echo "Sample data restored into $PGDATABASE.";
